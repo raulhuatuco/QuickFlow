04e21569a81a17985b6d47d5fda8a84ea03d60da
David Krepsky <david@akenge.com>
2015-12-28 22:21:49 -0200

    fixed main window size

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,23 @@
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+  
+  // Load splash screen.
   QPixmap pixmap(":/splash.png");
   QSplashScreen *splash = new QSplashScreen(pixmap);
   splash->show();
-
   qApp->processEvents();
+  
+  // Create main window.
   QuickFlow w;
   
+  // Set main window size accordingly to screen resolution.
+  w.setGeometry(QApplication::desktop()->screenGeometry());
+  
+  // Close splash screen and open main windows after 2500 msec.
   QTimer::singleShot(2500, splash, SLOT(close()));
   QTimer::singleShot(2500, &w, SLOT(show()));
   
+  // Process application messages.
   return a.exec();
 }
